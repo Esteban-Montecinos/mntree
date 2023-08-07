46eebe5534db34739b8b8e9c81081da44e6950f1
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-08-07 16:09:10 -0400

    Initial commit

---
+++ src/app/admin/layout.jsx
@@ -0,0 +1,10 @@
+export const metadata = {
+    title: 'Mntree Admin',
+    description: 'Mntree Admin',
+  }
+export default function AdminLayout({children}) {
+  
+  return (
+    children
+  ) 
+}
