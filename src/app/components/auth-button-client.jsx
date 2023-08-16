b249a65f62e9bc7bb6e82c8e12518e002df5a91e
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-08-16 03:06:05 -0400

    update url

--- src/app/components/auth-button-client.jsx
+++ src/app/components/auth-button-client.jsx
@@ -11,7 +11,7 @@ export function AuthButtonClient({ session }) {
     await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: "https://mntree.vercel.app/auth/callback",
       },
     });
   };
