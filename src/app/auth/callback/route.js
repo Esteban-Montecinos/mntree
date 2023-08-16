2c23eacf2f138fc24898f1cf8703f781054fc3bc
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-08-16 19:04:41 -0400

    eslint

--- src/app/auth/callback/route.js
+++ src/app/auth/callback/route.js
@@ -5,8 +5,7 @@ import { NextResponse } from 'next/server'
 export const dynamic = 'force-dynamic'
 
 export async function GET(request) {
- 
-    const requestUrl = new URL(request.url)
+  const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
 
   if (code) {
