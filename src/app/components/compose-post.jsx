9c2d36d4264e87c4a09e7eb1b564979cd56b22aa
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-08-16 19:48:03 -0400

    form action

--- src/app/components/compose-post.jsx
+++ src/app/components/compose-post.jsx
@@ -1,19 +1,30 @@
-'use client'
+import { cookies } from 'next/headers'
+import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
+import { revalidatePath } from 'next/cache'
 
-import { ComposePostButton } from './compose-post-button'
-import { addPost } from '../actions/add-post-action'
-import { useRef } from 'react'
+export const dynamic = 'force-dynamic'
 
 export function ComposePost ({
   userAvatarUrl = ''
 }) {
-  const formRef = useRef(null)
+
+const addPost = async (formData) => {
+  const content = formData.get('content')
+
+  if (content === null) return
+
+  const supabase = createServerActionClient({ cookies })
+  // revisar si el usuario realmene está autentificado
+  const { data: { user } } = await supabase.auth.getUser()
+  if (user === null) return
+
+  await supabase.from('posts').insert({ content, user_id: user.id })
+
+  revalidatePath(`/?content=${content.toString()}`)
+}
 
   return (
-    <form ref={formRef} action={async (formData) => {
-      await addPost(formData)
-      formRef.current?.reset()
-    }} className='flex flex-row p-3 border-b border-neutral-600 w-full'>
+    <form action={addPost} className='flex flex-row p-3 border-b border-neutral-600 w-full'>
       <img className='rounded-full w-10 h-10 object-contain mr-4' src={userAvatarUrl} alt="foto de perfil de GitHub"/>
       <div className='flex flex-1 flex-col gap-y-4'>
       <textarea
@@ -22,7 +33,13 @@ export function ComposePost ({
         className='w-full text-xl text-white bg-transparent resize-none outline-none border-b border-neutral-600 placeholder-gray-400'
         placeholder='¿Qué estas pensando?...'
       ></textarea>
-        <ComposePostButton />
+      <button
+      disabled={pending}
+      type='submit'
+      className='bg-neutral-900 text-white text-sm disabled:opacity-40 disabled:pointer-events-none font-bold rounded-full px-5 py-2 self-end'
+    >
+    Postear
+  </button>
       </div>
     </form>
   )
