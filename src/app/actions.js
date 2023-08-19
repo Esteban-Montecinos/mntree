9094e13f80bd81f0c62e60cbb1d9e0e433028532
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-08-18 22:20:41 -0400

    bug: null content

--- src/app/actions.js
+++ src/app/actions.js
@@ -1,19 +1,22 @@
-'use server'
+"use server";
 
-import { cookies } from 'next/headers'
-import { createServerActionClient } from '@supabase/auth-helpers-nextjs'
-import { revalidatePath } from 'next/cache'
+import { cookies } from "next/headers";
+import { createServerActionClient } from "@supabase/auth-helpers-nextjs";
+import { revalidatePath } from "next/cache";
 
 export async function addPost(content) {
 
-  if (content === null) return
+  if (content === "") return;
 
-  const supabase = createServerActionClient({ cookies })
+  const supabase = createServerActionClient({ cookies });
   // revisar si el usuario realmene está autentificado
-  const { data: { user } } = await supabase.auth.getUser()
-  if (user === null) return
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  
+  if (user === null) return;
 
-  await supabase.from('posts').insert({ content, user_id: user.id })
+  await supabase.from("posts").insert({ content, user_id: user.id });
 
-  revalidatePath('/')
+  revalidatePath("/");
 }
