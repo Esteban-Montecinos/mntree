5b8062e622be25366b7b54e98a5c39cbaca10206
Esteban-Montecinos <estebanmontecinos2001@gmail.com>
2023-08-18 21:21:14 -0400

    error content

--- src/app/actions.js
+++ src/app/actions.js
@@ -6,7 +6,7 @@ import { revalidatePath } from 'next/cache'
 
 export const dynamic = 'force-dynamic'
 
-export const addPost = async (content) => {
+export const addPost = async ({content}) => {
 
   if (content === null) return
 
